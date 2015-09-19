fd154c2a18799e619ca67d519424649d5f9afbd6
imamkurniawan <imamkurniawan.ntb@gmail.com>
2015-09-19 11:24:21 +0800

    versi 1.0.0

---
+++ clientImam.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class clientImam{
+	public static void main(String args[]) throws IOException{
+		String REMOTE_MESSAGE ="";
+		StringBuffer content = new StringBuffer();
+		if (args.length <= 2){
+			System.out.println("Pemakaian: java simpleClient <IP-ADDR> <PORT> <MESSAGE>");
+			System.exit(0);
+		}
+		try{
+			String REMOTE_IP = args[0];
+			int REMOTE_PORT = Integer.parseInt(args[1]); // ubah dari string ke integer
+			// Jika message lebih dari satu kata maka akan ditampung ; String REMOTE_MESSAGE = args[2];
+			for(int i = 2; i < args.length; i++){
+				REMOTE_MESSAGE = REMOTE_MESSAGE + " "+ args[i];
+			}
+			Socket server = new Socket(REMOTE_IP, REMOTE_PORT);
+			InputStream in = server.getInputStream();
+			OutputStream out = server.getOutputStream();
+			// write a byte
+			out.write(42);
+			// write a newline or carriage return delimited string
+			PrintWriter pout = new PrintWriter( out, true );
+			pout.println(REMOTE_MESSAGE);
+			// read a byte
+			byte back = (byte)in.read();
+			// read a newline or carriage return delimited string
+			BufferedReader bin = new BufferedReader( new InputStreamReader( in ) );
+			String response = bin.readLine();
+			//int jumBar = bin.count();
+			//System.out.println(jumBar);
+			//String response;
+			/*while((response = bin.readLine()) != null){
+				System.out.println(response);	
+				//content.append(response).append("\n");
+			}*/
+			//System.out.print(content.toString());
+			System.out.println(response);
+			// send a serialized Java object
+			ObjectOutputStream oout = new ObjectOutputStream( out );
+			//oout.writeObject( new java.util.Date() );
+			//oout.flush();
+			server.close();
+		}
+		catch(IOException e){
+			System.out.println("Error");
+		}
+	}
+
+}
---
+++ serverImam.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class serverImam{
+	public static void main(String args[]) throws IOException {
+		BufferedReader bufferedReader = null;
+		StringBuffer content = new StringBuffer();
+		if (args.length == 0){
+			System.out.println("Pemakaian: java serverImam <PORT>");
+			System.exit(0);
+		}
+		try {
+			int i = 0;
+			int TESTPORT = Integer.parseInt(args[0]);
+			System.out.println("Running in PORT "+ TESTPORT);
+			ServerSocket listener = new ServerSocket( TESTPORT );
+			while ( i<20 ) {
+			Socket client = listener.accept();  // wait for connection
+
+			InputStream in = client.getInputStream();
+			OutputStream out = client.getOutputStream();
+			
+			// read a byte
+			byte someByte = (byte)in.read();
+
+			// read a newline or carriage-return-delimited string
+			BufferedReader bin = new BufferedReader( new InputStreamReader( in ) );
+			String someString = bin.readLine();
+			System.out.println("Request From Client: " +someString);
+
+			// write a byte
+			out.write(43);
+
+			PrintWriter pout = new PrintWriter( out, true );
+			// if client message = "1" then will read a.txt
+			if(someString.compareTo(" 1") == 0){
+				try{
+					String text = null;
+					// open file a.txt
+					bufferedReader = new BufferedReader(new FileReader("a.txt"));
+					while((text = bufferedReader.readLine()) != null){
+						content.append(text).append("\n");
+					}
+				}
+				catch(FileNotFoundException e){
+					System.out.println("File Tak ditemukan!");
+				}
+				finally{
+					try{
+						if(bufferedReader != null)
+							bufferedReader.close();
+					}
+					catch(IOException ioe){
+						System.out.println("ioe");
+					}
+				}
+				System.out.print(content.toString());
+				pout.println(content.toString());
+			}
+			// if client message = "2" the will read b.txt
+			if(someString.compareTo(" 2") == 0){
+				//pout.println("Baca file 2");
+				try{
+					String text = null;
+					// open file a.txt
+					bufferedReader = new BufferedReader(new FileReader("b.txt"));
+					while((text = bufferedReader.readLine()) != null){
+						content.append(text).append("\n");
+					}
+				}
+				catch(FileNotFoundException e){
+					System.out.println("File Tak ditemukan!");
+				}
+				finally{
+					try{
+						if(bufferedReader != null)
+							bufferedReader.close();
+					}
+					catch(IOException ioe){
+						System.out.println("ioe");
+					}
+				}
+				System.out.print(content.toString());
+				pout.println(content.toString());	
+			}
+			else{
+				pout.println("Hello Fellas, pesan 1 untuk buka a.txt dan 2 untuk b.txt");
+				pout.println("# untuk membuka file a.txt \nCommand: java clientImam <IP> <PORT> 1");
+				pout.println("# untuk membuka file b.txt \nCommand: java clientImam <IP> <PORT> 2");
+			}
+			
+			//pout.println("Goodbye!");
+			// read a serialized Java object
+			ObjectInputStream oin = new ObjectInputStream( in );
+			//Date date = (Date)oin.readObject();
+			client.close();
+			i++;
+			}
+			//listener.close();
+		}
+		catch (IOException e ) { 
+			System.out.println("...");
+		}
+		//catch (ClassNotFoundException e2 ) { 
+		//	System.out.println("...");
+		//}
+	}
+}
